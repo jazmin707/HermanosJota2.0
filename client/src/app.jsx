ee8f0ed1e3737c9deb0db9cf4846d50b7d92c18c
fio <fionicassino@gmail.com>
2025-10-01 15:12:49 -0300

    AGREGO FORMULARIO A APP

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ContactForm from './components/ContactForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,11 +29,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <ContactForm />
     </>
   )
 }
 
 export default App
+
 
 /*
 
