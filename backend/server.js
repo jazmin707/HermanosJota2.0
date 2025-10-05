3aac9771c4cca40d0f0609f2a1206c70745ed8d1
csandobal <celestesandobal110@gmail.com>
2025-10-05 11:34:33 -0300

    modularizar error handler en middleware/errorHandler.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const productosRouter = require('./routes/productos');
-const logger = require('./middleware/logger'); // <-- nuevo import
+const logger = require('./middleware/logger'); 
 
 app.use(express.json());
-app.use(logger); // <-- nuevo uso del middleware
+app.use(logger); 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/productos', productosRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "Servidor Express funcionando 🚀" });
+});
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
+});
+
+const errorHandler = require('./middleware/errorHandler');
+app.use(errorHandler);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
